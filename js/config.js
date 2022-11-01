d8711739992d406d98d0f58e40c86eb09470a657
Sundar <sundar1100@gmail.com>
2022-11-01 14:29:16 +0530

    added the cognos client id and app id

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_sRfIuO4M7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '401m6f8lno8b7a20t14co0l61o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
